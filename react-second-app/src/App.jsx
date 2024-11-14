a78a69d2f822067f615a6f890d3ebffaf2dfcbce
Divyanshu Dhiman <154509898+divynashu001@users.noreply.github.com>
2024-11-14 13:57:50 +0000

    rendering lottery component

--- react-second-app/src/App.jsx
+++ react-second-app/src/App.jsx
@@ -2,12 +2,15 @@ import './App.css'
 // import LudoBoard from './LudoBoard'
 //import TodoList from './TodoList'
 import Lottery from './Lottery'
+//import TicketNum from './TicketNum'
+//import Ticket from './Ticket'
 
 function App() {
   
   return (
     <>
-      <Lottery/>
+      <Lottery n={3} winingSum={15}/>
+      
     </>
   )
 }
